315dd2feda3f0c47e7ec084c10e865b8732e7e6e
Anggi Edwarsa <anggiedwarsa@gmail.com>
2020-07-21 21:13:24 +0700

    Tambahkan bootstrap di Login

--- instamedia/src/Login.js
+++ instamedia/src/Login.js
@@ -3,18 +3,44 @@ import React, {Component} from "react";
 class Login extends Component {
     render() {
         return (
-            <div class="boxcontainer">
-                <form id="loginform">
-                    <label class="username">
-                        <span>Username or email</span>
-                        <input aria-label="Username or email" aria-required="true" name="username" type="text"/>
-                    </label>
-                    <label class="password">
-                        <span>Password</span>
-                        <input aria-label="Password" aria-required="true" name="password" type="password"/>
-                    </label>
-                    <button class="btnLogin" type="submit">Log In</button>
-                </form>
+            <div class="mt-5">
+                <div class="row justify-content-md-center">
+                    <div class="col-sm"></div>
+                    <div class="col-sm">
+                        <div class="card bg-light">
+                            <div class="border border-light p-3 mb-4">
+                                <div class="text-center">
+                                    <h1>My Logo</h1>
+                                </div>
+                            </div>
+                            <form id="loginform">
+                                <div class="formgroup col-xl-8 mx-auto text-center">
+                                    <div class="col-xs-2">
+                                        <input class="form-control mt-1" aria-label="Username or email" aria-required="true" id="username" name="username" type="text" placeholder="Username or email"/>
+                                        <input class="form-control mt-1" aria-label="Password" aria-required="true" id="password" name="password" type="password" placeholder="Password"/>
+                                        <div class="border border-light p-3 mb-4">
+                                            <div class="text-center">
+                                                <button class="btn btn-primary pl-5 pr-5" type="submit">Log In</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                            <div class="border border-light p-3 mb-4">
+                                <div class="text-center">
+                                    <small>Forgot password?</small>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card bg-light mt-3">
+                            <div class="border border-light p-3">
+                                <div class="text-center">
+                                    <span>Don't have an account? </span><a href="#">Sign Up</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
